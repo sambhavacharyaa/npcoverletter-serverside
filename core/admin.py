30d95ba6582430010182f24dd82682c9ca9b1e0a
Sambhav Acharya <work.sambhavacharya@gmail.com>
2026-07-18 02:34:49 +0545

    made form entry save in db and created privacy policy page

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import CoverLetterSubmission
 
-# Register your models here.
+
+@admin.register(CoverLetterSubmission)
+class CoverLetterSubmissionAdmin(admin.ModelAdmin):
+    list_display = ('full_domain', 'submission_type', 'name', 'email', 'created_at')
+    list_filter = ('submission_type', 'created_at')
+    search_fields = ('domain_name', 'name', 'email')
+    readonly_fields = ('submission_type', 'name', 'email', 'domain_name', 'domain_tld', 'created_at')
+
+    def has_add_permission(self, request):
+        return False
+
+    def full_domain(self, obj):
+        return obj.full_domain
+    full_domain.short_description = 'Domain'
--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
-
+from django.views.generic import TemplateView
 urlpatterns = [
     path('', views.home, name='home'),
     path("download-pdf/", views.download_pdf, name="download_pdf"),
@@ -15,4 +15,5 @@ urlpatterns = [
     
 ),
     path('download/jpeg/', views.download_jpeg, name='download_jpeg'),
+    path('privacy-policy/', TemplateView.as_view(template_name='privacy-policy.html'), name='privacy_policy'),
 ]
